97c8fb799e2153c5bf389589313fb5cb6fdb4f8d
Luke Green <elukuent@gmail.com>
2016-08-31 13:20:28 -0700

    bootstrapping
    initialized angular app, configged sass

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameAppOneApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gameAppOneApp
+ */
+angular.module('gameAppOneApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameAppOneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gameAppOneApp
+ */
+angular.module('gameAppOneApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
