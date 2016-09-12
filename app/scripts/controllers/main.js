1d2b5a64debfc97e64535192d4f9730ee0bc5faa
Luke Green <elukuent@gmail.com>
2016-09-12 14:26:04 -0700

    Quick update, new variable names
    Quick edits based on design advances.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,53 +8,58 @@
  * Controller of the gameAppOneApp
  */
 angular.module('gameAppOneApp')
-  .controller('MainCtrl', function ($scope, $route) {
+  .controller('MainCtrl', function ($scope) {
 
-      $scope.characterName = "Agnoxis";
-      $scope.characterType = "bard";
-      $scope.strength = 3;
-      $scope.intelligence = 9;
-      $scope.speed = 7;
-      $scope.dexterity = 11;
-      $scope.magic = 5;
+      $scope.characterName = "Beorn";
+      $scope.characterType = "Bear Walker";
+      $scope.health = 3;
+      $scope.attack = 9;
+      $scope.defense = 7;
+      $scope.agility = 11;
+      $scope.craft = 5;
+      $scope.morale = 2;
 
 
     //write a script that increases or descreases the attribute badge value when the
     //plus or minus button is clicked.  Only the 'active' attribute will be affected.
 
-    $scope.testScript = function($event){
+    $scope.setActiveItem = function($event){
       var activeListItem = this.activeItem;
       console.log(activeListItem);
-      }
+    };
 
     $scope.addOne = function(activeListItem){
-      //$scope.strength = $scope.strength + 1;
-      if(activeListItem === 'strength'){
-        $scope.strength = $scope.strength + 1;
-      } else if (activeListItem === 'intelligence') {
-        $scope.intelligence = $scope.intelligence + 1;
-      } else if (activeListItem === 'speed') {
-        $scope.speed = $scope.speed + 1;
-      } else if (activeListItem === 'dexterity') {
-        $scope.dexterity = $scope.dexterity + 1;
-      } else if (activeListItem === 'magic') {
-        $scope.magic = $scope.magic + 1;
+      //$scope.health = $scope.health + 1;
+      if(activeListItem === 'health'){
+        $scope.health = $scope.health + 1;
+      } else if (activeListItem === 'attack') {
+        $scope.attack = $scope.attack + 1;
+      } else if (activeListItem === 'defense') {
+        $scope.defense = $scope.defense + 1;
+      } else if (activeListItem === 'agility') {
+        $scope.agility = $scope.agility + 1;
+      } else if (activeListItem === 'craft') {
+        $scope.craft = $scope.craft + 1;
+      } else if (activeListItem === 'morale') {
+        $scope.morale = $scope.morale + 1;
       }
 
     };
 
     $scope.subtractOne = function(activeListItem){
-      //$scope.strength = $scope.strength - 1;
-      if(activeListItem === 'strength'){
-        $scope.strength = $scope.strength - 1;
-      } else if (activeListItem === 'intelligence') {
-        $scope.intelligence = $scope.intelligence - 1;
-      } else if (activeListItem === 'speed') {
-        $scope.speed = $scope.speed - 1;
-      } else if (activeListItem === 'dexterity') {
-        $scope.dexterity = $scope.dexterity - 1;
-      } else if (activeListItem === 'magic') {
-        $scope.magic = $scope.magic - 1;
+      //$scope.health = $scope.health - 1;
+      if(activeListItem === 'health'){
+        $scope.health = $scope.health - 1;
+      } else if (activeListItem === 'attack') {
+        $scope.attack = $scope.attack - 1;
+      } else if (activeListItem === 'defense') {
+        $scope.defense = $scope.defense - 1;
+      } else if (activeListItem === 'agility') {
+        $scope.agility = $scope.agility - 1;
+      } else if (activeListItem === 'craft') {
+        $scope.craft = $scope.craft - 1;
+      } else if (activeListItem === 'morale') {
+        $scope.morale = $scope.morale - 1;
       }
 
     };
